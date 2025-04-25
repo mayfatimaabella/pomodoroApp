1e77b54d776c1a1f53b3bc1d23c0ad923b77e771
May Fatima Abella <mayfatimabella@gmail.com>
2025-04-25 15:42:37 +0800

    partial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  time: string | any;
+  private interval : any;
 
   constructor() {}
+
+  ngOnInit() {
+    this.currentTime();
+    this.interval = setInterval(() => {
+      this.currentTime(); 
+    }, 1000);
+    
+  }
+
+  currentTime(){
+    const timeNow = new Date();
+    this.time = timeNow.toLocaleTimeString();
+  }
+
+  ngOnDestroy(){
+    clearInterval(this.interval);
+  }
 
 }
